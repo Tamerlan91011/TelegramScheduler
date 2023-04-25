f40069b620357f8870509f2e7be0cd38026c7c2e
Timur Glazunov <timurglazunov@mail.ru>
2023-04-25 11:37:46 +0300

    Изменил ORM модели

--- control_panel/shedule/models.py
+++ control_panel/shedule/models.py
@@ -2,18 +2,22 @@ from django.db import models
 
 from students.models import Group
 
-class Subject(models.Model):
-    name = models.CharField(max_length=256)
-    
+class LessonDate(models.Model):
+    date = models.DateField("Дата проведения занятия", auto_now=False, auto_now_add=False, null=True)
+
+class LessonType(models.Model):
+    name = models.CharField(max_length=128)
+
 class AcademicCouple(models.Model):
     start_time = models.TimeField()
     end_time = models.TimeField()
     
 class Lesson(models.Model):
     week = models.IntegerField()
-    weekday = models.IntegerField()
+    auditorium = models.CharField(max_length=256, default="Не назначена")
     
     academic_couple = models.ForeignKey(AcademicCouple, null=True, on_delete=models.SET_NULL)
-    
-    subject = models.ManyToManyField(Subject)
+    type_name = models.ForeignKey(LessonType, null=True, on_delete=models.SET_NULL)
+
     group = models.ManyToManyField(Group)
+    lesson_date = models.ManyToManyField(LessonDate)
--- control_panel/students/models.py
+++ control_panel/students/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Group(models.Model):
     name = models.CharField("Наименование", max_length=256)
 
@@ -8,6 +7,6 @@ class User(models.Model):
     name = models.CharField("ФИО", max_length=256)
     student_card_id = models.IntegerField("Номер студенческого билета")
     password = models.CharField("Пароль", max_length=256)
-    chat_id = models.IntegerField("Номер чата в телеграм")
+    chat_id = models.IntegerField("Номер чата в телеграм", null=True)
     
     group = models.ForeignKey(Group, null=True, on_delete=models.SET_NULL)
