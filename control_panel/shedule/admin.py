ea384f1151b933ba6f10d7b445a78024a2f35edb
Oleg Somov <gog0125@mail.ru>
2023-05-04 12:31:33 +0300

    добавил разделы из базы данных, сделал корректное отображение данных в каждом разделе

--- control_panel/shedule/admin.py
+++ control_panel/shedule/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Lesson, LessonDate, LessonType, AcademicCouple
 
 # Register your models here.
+
+class LessonDateAdmin(admin.ModelAdmin):
+    list_display = ["date"]
+
+
+class  LessonTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class AcademicCoupleAdmin(admin.ModelAdmin):
+    list_display = ["start_time", "end_time"]
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["week", "academic_couple_id", "type_name_id", "auditorium"]
+
+
+admin.site.register(LessonDate, LessonDateAdmin)
+admin.site.register(LessonType, LessonTypeAdmin)
+admin.site.register(AcademicCouple, AcademicCoupleAdmin)
+admin.site.register(Lesson, LessonAdmin)
--- control_panel/students/admin.py
+++ control_panel/students/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User,Group
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "student_card_id", "password", "chat_id", "group_id"]
+
+
+class UserGroup(admin.ModelAdmin):
+    list_display = ["name"]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, UserGroup)
