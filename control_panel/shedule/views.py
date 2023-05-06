1b08600748647d06e3f028f8805180074006f858
Timur Glazunov <timurglazunov@mail.ru>
2023-05-06 15:01:53 +0300

    REST запросы для получения ВСЕХ занятий, и занятия по его ID

--- control_panel/shedule/views.py
+++ control_panel/shedule/views.py
@@ -5,14 +5,20 @@ from rest_framework.response import Response
 from .models import Lesson
 from .serializers import *
         
-class Lessons(APIView):
-    def get(self, *args, **kwargs):
+class AllLessons(APIView):
+    def get(self, request):
         all_lessons = Lesson.objects.all()
         serialized_lessons = LessonSerializer(all_lessons, many=True)
         return Response(serialized_lessons.data)
+    
+class SingleLesson(APIView):    
+    def get(self, request, pk):
+        lesson = Lesson.objects.get(id=pk)
+        serialized_lessons = LessonSerializer(lesson)
+        return Response(serialized_lessons.data)
 
 class FirstLesson(APIView):
-    def get(self, *args, **kwargs):
+    def get(self):
         first_lesson = Lesson.objects.first()
         serialized__first_lesson = LessonSerializer(first_lesson)
         return Response(serialized__first_lesson.data)
